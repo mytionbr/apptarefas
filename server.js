9a2f75cfb59bc058d4e50d76f77e370726e46a3e
mytionBr <matheuss.jesus2017@gmail.com>
2021-01-23 13:31:40 -0300

    adding basic configuration

--- server.js
+++ server.js
@@ -1,14 +1,12 @@
 const express = require('express')
 const app = express()
-const fs = require('fs')
-app.use(express.static('client'))
 
-app.get("/",function(req,res){
-    fs.readFile(__dirname+'/client/index.html',function(err,data){
-        res.end(data)
-    })
+app.set('view engine', 'ejs')
+
+app.get('/', (req,res)=>{
+    res.render('tasks/index')
 })
 
-app.listen(process.env.PORT|| 5000,function(){
+app.listen(3000,function(){
     console.log("the server is starding")
 })
