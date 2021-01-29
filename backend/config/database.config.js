9751eb295008f3c111396f1926b99e17a63fd5a3
mytionBr <matheuss.jesus2017@gmail.com>
2021-01-29 14:42:38 -0300

    add configuration folder

---
+++ backend/config/database.config.js
@@ -0,0 +1,5 @@
+const { modelNames } = require("mongoose");
+
+module.exports = {
+    url: 'mongodb://localhost/blog'
+}
--- backend/server.js
+++ backend/server.js
@@ -3,17 +3,20 @@ const routers = require('./routers/tasks.js')
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
 
+const dbConfig = require('./config/database.config')
+const portConfig = require('./config/port.config')
+
 const app = express()
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 
-mongoose.connect('mongodb://localhost/blog',
+mongoose.connect(dbConfig.url,
 {useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true})
 .then(()=>console.log('database connected'))
 
 
 app.use('/tasks', routers)
 
-app.listen(3000,()=>console.log('server started'))
+app.listen(portConfig.port,()=>console.log(`Server is listening on PORT: ${portConfig.port}`))
