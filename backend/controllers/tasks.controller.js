7f60df37c98f9cb2a3b236de0608e3ba41240918
mytionBr <matheuss.jesus2017@gmail.com>
2021-01-30 02:55:35 -0300

    modifying the findAll method

--- backend/controllers/tasks.controller.js
+++ backend/controllers/tasks.controller.js
@@ -2,22 +2,32 @@ const Task = require('./../models/task.model')
 
 module.exports = {
     findAll: (req, res) => {
-        res.json(tasks)
+        Task.find()
+            .then(tasks => {
+                res.json(tasks)
+            })
+            .catch(err => {
+                res.status(500)
+                    .json({
+                        message: `Some error occurred: ${err.message}`
+                    })
+            }
+            )
     },
     findOne: (req, res) => {
         let id = Number(req.params.id)
-    
+
         let task = tasks.find(task => task._id === id)
         console.log(task)
         if (task) res.json(task)
         else res.json({ message: "object not found" })
-    
+
     },
     create: (req, res) => {
         // Validando se o usuário enviou o titulo
-        if(!req.body.title){
+        if (!req.body.title) {
             return res.status(400).json(
-                {message: "it was not possible to create a task due to lack of title"}
+                { message: "it was not possible to create a task due to lack of title" }
             )
         }
 
@@ -29,17 +39,18 @@ module.exports = {
             date: req.body.date,
             status: req.body.status,
             category: req.body.category
-        }) 
-        
+        })
+
         //salvando o objeto task no db
         task.save()
-        .then(task => res.json(task))
-        .catch(err => {
-            res.status(500).json(
-                {message: `Some error occurred: ${err.message}`}
-            )
-        })
+            .then(task => res.json(task))
+            .catch(err => {
+                res.status(500).json(
+                    { message: `Some error occurred: ${err.message}` }
+                )
+            })
     },
+
     update: (req, res) => {
         let id = Number(req.params.id)
         let taskUpdade = { ...req.body }
@@ -48,7 +59,7 @@ module.exports = {
             index = i
             return task._id === id
         })
-    
+
         if (task) {
             task = taskUpdade
             tasks[index] = task
@@ -59,7 +70,7 @@ module.exports = {
         }
     },
 
-    delete:(req,res)=>{
+    delete: (req, res) => {
         let id = Number(req.params.id)
         let index
         let task = tasks.find((task, i) => {
@@ -67,11 +78,11 @@ module.exports = {
             return task._id === id
         })
         console.log(index)
-        if(task){
-           tasks.splice(index,1)
-           res.json(tasks)
+        if (task) {
+            tasks.splice(index, 1)
+            res.json(tasks)
         }
-        else{
+        else {
             res.json({ message: 'Object not found' })
         }
     }
