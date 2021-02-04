446cfed358b7083100de15997474d7c1d8dafd0b
mytionBr <matheuss.jesus2017@gmail.com>
2021-02-04 13:08:48 -0300

    adding an algorithm to sort tasks by category

--- frontend/views/public/js/logic/interface.js
+++ frontend/views/public/js/logic/interface.js
@@ -1,1 +1,52 @@
-let 
+document.addEventListener('DOMContentLoaded',()=>{
+    criaLinksSidebar()
+})
+
+let tasks = [{
+    title: 'Hello',
+    category: 'Work'
+},
+{
+    title: 'Hello',
+    category: 'Work'
+},
+{
+    title: 'Hsdf',
+    category: 'Scholl'
+}]
+
+function criaLinksSidebar(){
+    let container_links = document.querySelector('.container-links')
+    
+    let div = document.createElement('div')
+    div.classList.add('list-tasks-links') 
+    
+    let elementoOrdenado = ordenaTasksPorCategoria(tasks)
+    console.log(elementoOrdenado)
+    
+}
+
+function ordenaTasksPorCategoria(array){
+    
+        let categorias = {}
+       
+        array.forEach(elemento=>{
+            //verifica se o objeto está vazio
+          if(categorias.length === 0){
+            //se estiver, então crie uma propriedade com o valor da
+            // categoria
+            categorias[elemento.category] = [elemento]
+          }else if(categorias.hasOwnProperty(elemento.category)){
+              //verifica se o objeto já tem uma propriedade com o valor
+              // da categoria, se sim, então adicione o elemento no array
+              // da propriedade
+              categorias[elemento.category].push(elemento)
+          }else{
+            // Se não tiver, então crie uma propriedade com o valor da
+            // categoria 
+            categorias[elemento.category] = [elemento]
+          }
+          
+        })
+        return categorias   
+}
